9f530b906ecb9f01df55f255c210b2ab35fc0d16
Fauzi Ihsan <fauzyihsan@gmail.com>
2020-09-26 19:05:05 +0700

    membuat repo DIA

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.close').click(function(){
+			// console.log('ihsan');
+			   // setTimeout(function(){
+			   //     location.reload();
+			   // },1);
+
+			   window.location.reload();
+
+	});
+
+	
+
+});
